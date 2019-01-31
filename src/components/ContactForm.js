b6ba9eb67b8c4e9baa00ce6c29e29a96c3767ca9
Collin Argo <collinargo@gmail.com>
2019-01-31 16:52:40 -0500

    style contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,22 +2,24 @@ import React from 'react'
 
 export default ({images}) => (
     <form className='Contact'>
-        <h1>contact</h1>
-        <label>name
-            <input type='text' name='name' id='name'/>
-        </label>
-        <label>e-mail
-            <input type='text' name='email' id='email'/>
-        </label>
-        <label>message
-            <input type='text' name='message' id='message'/>
-        </label>
-        <label>artwork
-            <select name='artwork' id='artwork'>
-                <option value='-'> - </option>
-                {images.map((image, index)=> <option key={index} value={image.title}>{image.title}</option>)}
-            </select>
-        </label>
-        <input type='submit' value='submit'/>
+        <h1 className='formTitle'>contact</h1>
+        <div className='formInputs'>
+            <label>name
+                <input type='text' name='name' id='name'/>
+            </label>
+            <label>e-mail
+                <input type='text' name='email' id='email'/>
+            </label>
+            <label>message
+                <input type='text' name='message' id='message'/>
+            </label>
+            <label>artwork
+                <select name='artwork' id='artwork'>
+                    <option value='-'> - </option>
+                    {images.map((image, index)=> <option key={index} value={image.title}>{image.title}</option>)}
+                </select>
+            </label>
+            <input type='submit' value='submit'/>
+        </div>
     </form>
 )
