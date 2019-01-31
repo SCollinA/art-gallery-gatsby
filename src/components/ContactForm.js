4ea969fac3bd9d17618ee53cd22ab68dfb148edb
Collin Argo <collinargo@gmail.com>
2019-01-31 15:56:52 -0500

    frame contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default () => (
-    <div>
-        
-    </div>
+export default ({images}) => (
+    <form onSubmit={() => null}>
+        <input type='text' />
+        <input type='text' />
+        <input type='text' />
+        <select name='artwork'>
+            {/* {images.map(image => <option />)} */}
+        </select>
+        <input type='submit' value='submit'/>
+    </form>
 )
