9379d83222a923be4501dbf6056238da9af72a83
Collin Argo <collinargo@gmail.com>
2019-01-31 13:56:38 -0500

    add components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Gallery from '../components/Gallery'
+import SocialLinks from '../components/SocialLinks'
+import ContactForm from '../components/ContactForm'
+import About from '../components/About'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    
+    <Gallery />
+    <SocialLinks />
+    <ContactForm />
+    <About />
   </Layout>
 )
 
