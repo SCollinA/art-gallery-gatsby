6d7d59668f243c540db27dabe91656a2a7d17c38
Collin Argo <collinargo@gmail.com>
2019-02-07 18:49:57 -0500

    add left and right arrows to gallery

--- src/components/GalleryThumbs.js
+++ src/components/GalleryThumbs.js
@@ -3,12 +3,14 @@ import Img from 'gatsby-image'
 
 export default ({images, selectImage}) => (
     <div className='GalleryThumbs'>
+        <h1 className='galleryArrowLeft'>{'<'}</h1>
         {images.map((image, index) => (
             <div key={index} className='galleryThumb'
-                onClick={() => selectImage(image)}
+            onClick={() => selectImage(image)}
             >
                 <Img fluid={image.image}/>
             </div>
         ))}
+        <h1 className='galleryArrowRight'>{'>'}</h1>
     </div>
 )
