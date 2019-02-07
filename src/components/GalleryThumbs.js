e3b20cdb3855c27da99c08df5b3d00b71d1ce6ce
Collin Argo <collinargo@gmail.com>
2019-02-07 18:52:09 -0500

    move arrows outside thumbnail

--- src/components/GalleryThumbs.js
+++ src/components/GalleryThumbs.js
@@ -4,13 +4,15 @@ import Img from 'gatsby-image'
 export default ({images, selectImage}) => (
     <div className='GalleryThumbs'>
         <h1 className='galleryArrowLeft'>{'<'}</h1>
-        {images.map((image, index) => (
-            <div key={index} className='galleryThumb'
-            onClick={() => selectImage(image)}
-            >
-                <Img fluid={image.image}/>
+            <div className='galleryThumbs'>
+                {images.map((image, index) => (
+                    <div key={index} className='galleryThumb'
+                    onClick={() => selectImage(image)}
+                    >
+                        <Img fluid={image.image}/>
+                    </div>
+                ))}
             </div>
-        ))}
         <h1 className='galleryArrowRight'>{'>'}</h1>
     </div>
 )
