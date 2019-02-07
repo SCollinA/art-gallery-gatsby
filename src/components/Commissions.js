cb1d20252c45e26b198f558200f5a7b8c9cbb2c7
Collin Argo <collinargo@gmail.com>
2019-02-07 18:42:13 -0500

    comment out unused imports

--- src/components/Commissions.js
+++ src/components/Commissions.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Img from 'gatsby-image'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { StaticQuery, graphql } from 'gatsby';
 
 export default ({image}) => (
