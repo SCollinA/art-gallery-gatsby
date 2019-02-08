fb8092dbfaaa1b11c6b09fa72d96e9a9ea1437b5
Collin Argo <collinargo@gmail.com>
2019-02-07 19:04:51 -0500

    fix gallery arrow icons

--- src/components/GalleryThumbs.js
+++ src/components/GalleryThumbs.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Img from 'gatsby-image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default ({images, selectImage}) => (
     <div className='GalleryThumbs'>
-        <h1 className='galleryArrowLeft'>{'<'}</h1>
-            <div className='galleryThumbs'>
-                {images.map((image, index) => (
-                    <div key={index} className='galleryThumb'
-                    onClick={() => selectImage(image)}
-                    >
-                        <Img fluid={image.image}/>
-                    </div>
-                ))}
-            </div>
-        <h1 className='galleryArrowRight'>{'>'}</h1>
+        <div className='galleryArrow'>
+            <FontAwesomeIcon size='2x' icon={['fas', 'angle-left']}/>
+        </div>
+        {/* <h1 className='galleryArrow'>{'<'}</h1> */}
+        <div className='galleryThumbs'>
+            {images.map((image, index) => (
+                <div key={index} className='galleryThumb'
+                onClick={() => selectImage(image)}
+                >
+                    <Img fluid={image.image}/>
+                </div>
+            ))}
+        </div>
+        {/* <h1 className='galleryArrow'>{'>'}</h1> */}
+        <div className='galleryArrow'>
+            <FontAwesomeIcon size='2x' icon={['fas', 'angle-right']}/>
+        </div>
     </div>
 )
