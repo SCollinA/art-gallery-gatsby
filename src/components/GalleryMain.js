f7db9266a87b102fb190489e26018096a0690dc4
Amelia Schulz <amelia.schulz@gmail.com>
2019-02-10 10:03:56 -0500

    adjusts gallery main

--- src/components/GalleryMain.js
+++ src/components/GalleryMain.js
@@ -4,7 +4,7 @@ import Img from 'gatsby-image'
 export default ({images, selectedImage}) => (
     <div className='GalleryMain'>
         <h1>{selectedImage.title}</h1>
-        {images.map((image, index) => <Img key={index} style={{
+        {images.map((image, index) => <Img className= "MainImg" key={index} style={{
             position: `${selectedImage.title === image.title ? 'relative' : 'fixed'}`,
             visibility: `${selectedImage.title === image.title ? 'visible' : 'hidden'}`
         }} fluid={image.image} />)}
