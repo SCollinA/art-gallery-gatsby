fc06b006ca3e6bc78be806e0bdb1e3667f542cb9
Collin Argo <collinargo@gmail.com>
2019-02-19 14:02:34 -0500

    fix gallery select artwork

--- src/components/GalleryMain.js
+++ src/components/GalleryMain.js
@@ -2,25 +2,25 @@ import React from 'react'
 import Img from 'gatsby-image'
 import { galleryContext } from './layout'
 
-export default ({ selectedArtwork }) => (
+export default ({ selectedArtworkTitle }) => (
     <div className='GalleryMain'>
         <galleryContext.Consumer>
             {({ artworks }) => {
-                const visibleArtwork = selectedArtwork || artworks[0]
-                console.log(artworks, visibleArtwork)
+                const visibleArtworkTitle = selectedArtworkTitle || artworks[0].name
+                console.log(artworks, visibleArtworkTitle)
                 return (
                     <>
-                        <h1>{artworks && visibleArtwork.name}</h1>
+                        <h1>{artworks && visibleArtworkTitle}</h1>
                         {artworks.map((artwork, index) => (
                             <Img key={index} style={{
-                                position: `${visibleArtwork.name === artwork.name ? 'relative' : 'fixed'}`,
-                                visibility: `${visibleArtwork.name === artwork.name ? 'visible' : 'hidden'}`
+                                position: `${visibleArtworkTitle === artwork.name ? 'relative' : 'fixed'}`,
+                                visibility: `${visibleArtworkTitle === artwork.name ? 'visible' : 'hidden'}`
                                 }} 
                                 fluid={artwork.childImageSharp.fluid} 
                                 // alt='artwork'
                             />
                         ))}
-                        <p>{visibleArtwork.medium}</p>
+                        <p>stuff will go here</p>
                     </>
                 )
             }}
