699608af33f86062106f957278f01e9ad94fa432
Collin Argo <collinargo@gmail.com>
2019-02-20 10:44:03 -0500

    add state to admin page

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,17 +1,37 @@
 import React from "react"
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import AddArtworks from "../components/AddArtworks";
-import AddGalleries from "../components/AddGalleries";
+import AddArtworks from "../components/AddArtworks"
+import AddGalleries from "../components/AddGalleries"
+import UpdateGalleryForm from '../components/UpdateGalleryForm'
+import UpdateArtworkForm from '../components/UpdateArtworkForm'
 
+class AdminPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isUpdating: false,
+            updatingGallery: {},
+            updatingArtwork: {},
+        }
+    }
 
-const AdminPage = () => {
-  return (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <AddGalleries/>
-    <AddArtworks/>
-  </Layout>
-)}
+    componentDidMount() {
+
+    }
+
+    render() {
+      return (
+            <Layout>
+                <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+                <AddGalleries/>
+                <AddArtworks/>
+                {this.state.isUpdating && 
+                    ((updatingGallery && <UpdateGalleryForm/>) || 
+                        (updatingArtwork && <UpdateArtworkForm/>))}
+            </Layout>
+        )
+    }
+}
 
 export default AdminPage
