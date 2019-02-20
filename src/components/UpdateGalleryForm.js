8bcd455d2ea68c48642573eee73f7fd4e2247c20
Collin Argo <collinargo@gmail.com>
2019-02-20 13:45:36 -0500

    build update gallery form

--- src/components/UpdateGalleryForm.js
+++ src/components/UpdateGalleryForm.js
@@ -1,23 +1,43 @@
 import React from 'react'
-// import { Mutation, gql } from 'apollo-boost'
+import { Mutation, gql } from 'apollo-boost'
 import { AdminContext } from '../pages/admin';
 
 export default UpdateGalleryForm = () => {
     return (
         <AdminContext.Consumer>
             {({ updatingGallery, selectGallery, changeGallery, submitGallery }) => (
-                <form>
-                    <input/>
-                    <input/>
-                    <input/>
-                    <input/>
-                    <input type='submit'/>
-                </form>
+                <Mutation mutation={UPDATE_GALLERY}>
+                    {(updateGallery, { data, loading, error }) => (
+                        <form
+                            onSubmit={event => {
+                                event.preventDefault()
+                                submitGallery(updatingGallery)
+                                updateGallery(updatingGallery)
+                            }}
+                        >
+                            <input type='text' name='name'
+                                value={updatingGallery.name}
+                                onChange={event => changeGallery({
+                                    ...updatingGallery,
+                                    name: event.target.value
+                                })}
+                            />
+                            <input type='submit' value='submit'/>
+                            <input type='reset' value='reset'/>
+                        </form>
+                    )}
+                </Mutation>
             )}
         </AdminContext.Consumer>
     )
 }
 
-// const UPDATE_GALLERY = gql`
-
-// `
+const UPDATE_GALLERY = gql`
+    mutation UpdateGallery($ID: String, $input: {
+        id: String,
+        name: String
+    }) {
+        id
+        name
+    }
+`
