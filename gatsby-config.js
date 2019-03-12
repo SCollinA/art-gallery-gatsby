641e5066424253760510e3e9cbcdeef6e8284846
Collin Argo <collinargo@gmail.com>
2019-03-12 14:52:17 -0400

    make ralphie the favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/brand_favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ralph_favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
