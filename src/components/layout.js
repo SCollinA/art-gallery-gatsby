a02459dcf13b349a552c196c88500c31a503cb12
Collin Argo <collinargo@gmail.com>
2019-03-14 13:46:19 -0400

    use query in right place

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { graphql, StaticQuery } from "gatsby"
-import { Query } from "react-apollo"
+import { Query, ApolloConsumer } from "react-apollo"
 import gql from 'graphql-tag'
 import LayoutContext from '../contexts/LayoutContext'
 import Header from "./header"
@@ -26,51 +26,59 @@ const Layout = ({ children }) => (
                 console.log('running artwork files static query', edges.map(({ node: { name }}) => name))
                 const artworkFiles = data.artworkFiles ? data.artworkFiles.edges.map(edge => edge.node) : []
                 return (
-                  <LayoutContext.Provider 
-                    value={{ 
-                      // if galleries has a gallery, add it's artworks
-                      galleries: galleries.length > 0 ? 
-                        galleries.map(gallery => {
-                          const galleryArtworks = artworks.filter(artwork => artwork.galleryId === gallery.id)
-                          const artworksWithFiles = galleryArtworks.length > 0 ? 
-                            galleryArtworks.map(({ id, galleryId, title, width, height, image, medium, price, sold }) => {
-                              // if an artwork file exist add it
-                              // will check if file is there to determine proper element for image
+                  <ApolloConsumer>
+                    {client => (
+                      <LayoutContext.Provider 
+                        value={{ 
+                          // if galleries has a gallery, add it's artworks
+                          galleries: galleries.length > 0 ? 
+                            galleries.map(gallery => {
+                              const galleryArtworks = artworks.filter(artwork => artwork.galleryId === gallery.id)
+                              const artworksWithFiles = galleryArtworks.length > 0 ? 
+                                galleryArtworks.map(({ id, galleryId, title, width, height, image, medium, price, sold }) => {
+                                  // if an artwork file exist add it
+                                  // will check if file is there to determine proper element for image
+                                  return {
+                                    id,
+                                    galleryId,
+                                    title,
+                                    width,
+                                    height,
+                                    image,
+                                    medium,
+                                    price,
+                                    sold,
+                                    file: artworkFiles.find(artworkFile => artworkFile.name === `${id}-${title}`),
+                                  }
+                                  // only include art that has a picture to show for the gallery
+                                }) :
+                                [{ id: 'nada', title: 'no artworks'}]
+                              artworksWithFiles.filter(artwork => !artwork.file).map(async filelessArtwork => {
+                                const { data: { getArtwork: { image }}} = await client.query({ query: ARTWORK_IMAGE, variables: { id: filelessArtwork.id } })
+                                return { ...filelessArtwork, image }
+                              })
+                              //   (
+                              //   <Query query={ARTWORK_IMAGE} variables={{ id: filelessArtwork.id }}>
+                              //     {({ data: { artworkImage: { image }}, loading, error }) => filelessArtwork.image = image }}
+                              //   </Query>
+                              // ))
                               return {
-                                id,
-                                galleryId,
-                                title,
-                                width,
-                                height,
-                                image,
-                                medium,
-                                price,
-                                sold,
-                                file: artworkFiles.find(artworkFile => artworkFile.name === `${id}-${title}`),
+                                id: gallery.id,
+                                name: gallery.name,
+                                artworks: artworksWithFiles,
                               }
-                              // only include art that has a picture to show for the gallery
-                            }).filter(artwork => artwork.file) :
-                            [{ id: 'nada', title: 'no artworks'}]
-                          artworksWithFiles.filter(artwork => !artwork.file).forEach(filelessArtwork => (
-                            <Query query={ARTWORK_IMAGE} variables={{ id: filelessArtwork.id }}>
-                              {({ data: { artworkImage: { image }}, loading, error }) => filelessArtwork.image = image }}
-                            </Query>
-                          ))
-                          return {
-                            id: gallery.id,
-                            name: gallery.name,
-                            artworks: artworksWithFiles,
-                          }
-                        }) : [{ 
-                          id: 'none', 
-                          name: 'no galleries', 
-                          artworks: [{ id: 'nada', title: 'no galleries #1'}]
-                        }],
-                    }}
-                  >
-                    {loading && <Loading/>}
-                    {children}
-                  </LayoutContext.Provider>
+                            }) : [{ 
+                              id: 'none', 
+                              name: 'no galleries', 
+                              artworks: [{ id: 'nada', title: 'no galleries #1'}]
+                            }],
+                        }}
+                      >
+                        {loading && <Loading/>}
+                        {children}
+                      </LayoutContext.Provider>  
+                    )}
+                  </ApolloConsumer>
                 )
               }}
             />
@@ -109,7 +117,7 @@ export const DB_CONTENT = gql`
 `
 
 export const ARTWORK_IMAGE = gql`
-  query GetArtworkImage($id: ID) {
+  query GetArtworkImage($id: ID!) {
       getArtwork(id: $id) {
         image
       }
