31be261cb968af207ceef988a1b58201dd1288db
Collin Argo <collinargo@gmail.com>
2019-04-05 09:33:07 -0400

    go back to tracedSVG

--- src/components/layout.js
+++ src/components/layout.js
@@ -203,7 +203,7 @@ export const fluidImage = graphql`
   fragment fluidImage on File {
     childImageSharp {
       fluid(maxWidth: 3000, quality: 100, srcSetBreakpoints: [200, 340, 520, 890]) {
-        ...GatsbyImageSharpFluid_noBase64
+        ...GatsbyImageSharpFluid_tracedSVG
       }
     }
   }
@@ -213,7 +213,7 @@ export const fixedImage = graphql`
   fragment fixedImage on File {
     childImageSharp {
       fixed(width: 3000, quality: 100) {
-        ...GatsbyImageSharpFixed_noBase64
+        ...GatsbyImageSharpFixed_tracedSVG
       }
     }
   }
