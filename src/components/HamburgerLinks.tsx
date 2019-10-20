52e5f157fbcf906a7bea27ce36e954898deba894
Collin Argo <collinargo@gmail.com>
2019-10-20 17:08:49 -0400

    style all the pages with media queries included

--- src/components/HamburgerLinks.tsx
+++ src/components/HamburgerLinks.tsx
@@ -1,33 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+
 import PageLinks from "./PageLinks";
+import SectionWrapper from "./SectionWrapper";
 import SocialLinks from "./SocialLinks";
 
-export default class HamburgerLinks extends React.Component<any, any, any> {
-	constructor(props: any) {
-		super(props);
-		this.state = {
-			isExpanded: false,
-		};
-	}
-
-	public render() {
-		return (
-			<div className={`HamburgerLinks${this.state.isExpanded ? " expandedMenu" : ""}`}
-				onClick={() => {
-					this.setState({
-						isExpanded: !this.state.isExpanded,
-					});
-				}}
-			>
-				<FontAwesomeIcon icon={["far", "times-circle"]} size="3x" />
-				{this.state.isExpanded && (
-					<>
-						<PageLinks />
-						<SocialLinks />
-					</>
-				)}
-			</div>
-		);
-	}
-}
+export default () => {
+	const [isExpanded, setIsExpanded] = React.useState(false);
+	return (
+		<div className={`HamburgerLinks${isExpanded ? " expandedMenu" : ""}`}
+			onClick={() =>
+				setIsExpanded(!isExpanded)}
+		>
+			<FontAwesomeIcon icon={["far", "times-circle"]} size="3x" />
+			{isExpanded &&
+				<SectionWrapper>
+					<PageLinks />
+					<SocialLinks />
+				</SectionWrapper>
+			}
+		</div>
+	);
+};
