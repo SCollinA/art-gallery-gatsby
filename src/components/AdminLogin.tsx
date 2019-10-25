0c79383afb1ca9596df025b08e57aa43bf21ec6d
Collin Argo <collinargo@gmail.com>
2019-10-24 20:03:51 -0400

    make login directly access admin context

--- src/components/AdminLogin.tsx
+++ src/components/AdminLogin.tsx
@@ -1,41 +1,55 @@
 import gql from "graphql-tag";
 import React from "react";
 import { Mutation } from "react-apollo";
+
+import AdminContext from "../contexts/adminContext";
+
 import Loading from "./Loading";
 
-export default ({ isAdminLoggedIn }: any) => (
-	<Mutation mutation={ADMIN_LOGIN}
-		onCompleted={({ login: { token }}: any) => {
-			localStorage.setItem("auth-token", token);
-			isAdminLoggedIn(true);
-		}}
-		onError={(err: any) => window.alert(err.message)}
-	>
-		{(adminLogin: any, { loading }: any) => (
-			<Loading loading={loading}>
-				<form className="AdminLogin"
-					onSubmit={(event: any) => {
-						event.preventDefault();
-						adminLogin({
-							variables: {
-								adminPassword: event.target.adminPassword.value,
-							},
-						});
-					}}
-				>
-					<label style={{ display: "none" }}>admin password
-						<input autoComplete={"username"} type="text" name="adminUsername" placeholder="no username"/>
-					</label>
-					<label>admin password
-						<input autoFocus autoComplete={"current-password"} type="password" placeholder="not 'password1'" name="adminPassword"/>
-					</label>
-					<label>submit
-						<input type="submit" value="proceed"/>
-					</label>
-				</form>
-			</Loading>
+export default () => (
+	<AdminContext.Consumer>
+		{({ isLoggedIn, setIsLoggedIn }: any) => (
+			<Mutation mutation={ADMIN_LOGIN}
+				onCompleted={({ login: { token }}: any) => {
+					localStorage.setItem("auth-token", token);
+					setIsLoggedIn(true);
+				}}
+				onError={(err: any) => window.alert(err.message)}
+			>
+				{(adminLogin: any, { loading }: any) => (
+					<Loading loading={loading}>
+						{isLoggedIn ?
+							<div className="logout">
+								<input type="button" value="logout"
+									onClick={() => setIsLoggedIn(false)}
+								/>
+							</div> :
+							<form className="AdminLogin"
+								onSubmit={(event: any) => {
+									event.preventDefault();
+									adminLogin({
+										variables: {
+											adminPassword: event.target.adminPassword.value,
+										},
+									});
+								}}
+							>
+								<label style={{ display: "none" }}>admin password
+									<input autoComplete={"username"} type="text" name="adminUsername" placeholder="no username"/>
+								</label>
+								<label>admin password
+									<input autoFocus autoComplete={"current-password"} type="password" placeholder="not 'password1'" name="adminPassword"/>
+								</label>
+								<label>submit
+									<input type="submit" value="proceed"/>
+								</label>
+							</form>
+						}
+					</Loading>
+				)}
+			</Mutation>
 		)}
-	</Mutation>
+	</AdminContext.Consumer>
 );
 
 const ADMIN_LOGIN = gql`
