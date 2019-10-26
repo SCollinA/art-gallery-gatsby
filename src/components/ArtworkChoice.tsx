98aa26a726098a6eeaa80a4fa48255e6293aa961
Collin Argo <collinargo@gmail.com>
2019-10-26 16:53:18 -0400

    add admin context to artwork choice

--- src/components/ArtworkChoice.tsx
+++ src/components/ArtworkChoice.tsx
@@ -1,5 +1,9 @@
 import { get } from "lodash/fp";
 import React from "react";
+
+import AdminContext from "../contexts/adminContext";
+
+import AddArtworks from "./AddArtworks";
 import ArtworkImage from "./ArtworkImage";
 import SectionWrapper from "./SectionWrapper";
 
@@ -11,24 +15,30 @@ export default ({
 }: any) => {
 	const artworks = get("artworks", selectedGallery) || [];
 	return (
-		<div className="ArtworkChoice" ref={artworkChoiceRef}>
-			<div className="artworksTitle">
-				<h3>{selectedGallery.name}</h3>
-				<h3>artworks</h3>
-			</div>
-			<SectionWrapper>
-				<div id="artworkThumbs">
-					{artworks.map((artwork: any, index: any) => (
-						<div key={index}
-							className={`artworkThumb${(selectedArtwork && artwork.id === selectedArtwork.id) ? " selectedArtwork" : ""}`}
-							onClick={() => selectArtwork(artwork)}
-						>
-							<ArtworkImage artwork={artwork}/>
-							<p>{artwork.title}</p>
+		<AdminContext.Consumer>
+			{({ isLoggedIn }: any) => (
+				<div className="ArtworkChoice" ref={artworkChoiceRef}>
+					<div className="artworksTitle">
+						<h3>{selectedGallery.name}</h3>
+						<h3>artworks</h3>
+					</div>
+					<SectionWrapper>
+						<div id="artworkThumbs">
+							{artworks.map((artwork: any, index: any) => (
+								<div key={index}
+									className={`artworkThumb${(selectedArtwork && artwork.id === selectedArtwork.id) ? " selectedArtwork" : ""}`}
+									onClick={() => selectArtwork(artwork)}
+								>
+									<ArtworkImage artwork={artwork}/>
+									<p>{artwork.title}</p>
+								</div>
+							))}
+							{isLoggedIn &&
+								<AddArtworks/>}
 						</div>
-					))}
+					</SectionWrapper>
 				</div>
-			</SectionWrapper>
-		</div>
+			)}
+		</AdminContext.Consumer>
 	);
 };
