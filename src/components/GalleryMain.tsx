52c0afe95592f4b61f20643a8d3d133d3e9c65db
Collin Argo <collinargo@gmail.com>
2019-10-26 16:54:47 -0400

    add admin context to gallery main

--- src/components/GalleryMain.tsx
+++ src/components/GalleryMain.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import AdminContext from "../contexts/adminContext";
 import LayoutContext from "../contexts/layoutContext";
 
 import ArtworkImage from "./ArtworkImage";
@@ -10,32 +11,40 @@ export default ({ galleryMainRef, selectedGallery, selectedArtwork }: any) => {
 	<div className="GalleryMain" ref={galleryMainRef}>
 		<LayoutContext.Consumer>
 			{({ galleries }: any) => (
-				<div className="selectedGallery">
-					<div className="galleryTitle">
-						<h2>{selectedGallery.name}</h2>
-						<h1>{selectedArtwork.title}</h1>
-					</div>
-					<SectionWrapper>
-						<div className="galleryImage">
-						{galleries.map(({ artworks }: any) =>
-							artworks.map((artwork: any, index: any) =>
-								<div key={index}
-									className={`galleryArtwork${selectedArtwork.id === artwork.id ? " current" : " hidden"}`}
-								>
-									<ArtworkImage artwork={artwork}/>
-								</div>,
-						))}
-					</div>
-					</SectionWrapper>
-					<div className="galleryCaption">
-						{!(selectedArtwork.width && selectedArtwork.height) || <p>{`W ${selectedArtwork.width} x H ${selectedArtwork.height}`}</p>}
-						<p>{selectedArtwork.medium}</p>
-						{selectedArtwork.framed && <p>framed</p>}
-						{!selectedArtwork.price || <p>{`$${selectedArtwork.price}`}</p>}
-						{selectedArtwork.sold && <p>sold</p>}
-						{/* this one will be a caption */}
-					</div>
-				</div>
+				<AdminContext.Consumer>
+					{({ isLoggedIn, editArtwork }: any) => (
+						<div className="selectedGallery">
+							<div className="galleryTitle">
+								<h2>{selectedGallery.name}</h2>
+								{isLoggedIn &&
+									<h2 onClick={() => editArtwork(selectedArtwork)}>
+										edit
+									</h2>}
+								<h1>{selectedArtwork.title}</h1>
+							</div>
+							<SectionWrapper>
+								<div className="galleryImage">
+								{galleries.map(({ artworks }: any) =>
+									artworks.map((artwork: any, index: any) => (
+										<div key={index}
+											className={`galleryArtwork${selectedArtwork.id === artwork.id ? " current" : " hidden"}`}
+										>
+											<ArtworkImage artwork={artwork}/>
+										</div>
+								)))}
+							</div>
+							</SectionWrapper>
+							<div className="galleryCaption">
+								{!(selectedArtwork.width && selectedArtwork.height) || <p>{`W ${selectedArtwork.width} x H ${selectedArtwork.height}`}</p>}
+								<p>{selectedArtwork.medium}</p>
+								{selectedArtwork.framed && <p>framed</p>}
+								{!selectedArtwork.price || <p>{`$${selectedArtwork.price}`}</p>}
+								{selectedArtwork.sold && <p>sold</p>}
+								{/* this one will be a caption */}
+							</div>
+						</div>
+					)}
+				</AdminContext.Consumer>
 			)}
 		</LayoutContext.Consumer>
 	</div>
