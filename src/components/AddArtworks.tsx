81d9d098606adce2fedee8132dec79df0ff7fc94
Collin Argo <collinargo@gmail.com>
2019-10-26 16:52:32 -0400

    sort things alphabetically

--- src/components/AddArtworks.tsx
+++ src/components/AddArtworks.tsx
@@ -8,83 +8,83 @@ import { DB_CONTENT } from "../graphql/graphql";
 import { GALLERY_ARTWORKS } from "./AdminArtworks";
 
 export default () => {
-    return (
-        <AdminContext.Consumer>
-            {({ selectArtwork, selectedGallery }: any) => (
-                    <Mutation mutation={ADD_ARTWORK}
-                        variables={{
-                            galleryId: selectedGallery.id || null,
-                        }}
-                        update={(cache: any, { data: { addArtwork } }: any) => {
-                            // get only needed variables, i.e. no '__typename'
-                            const {
-                                id,
-                                galleryId,
-                                title,
-                                width,
-                                height,
-                                image,
-                                medium,
-                                price,
-                                framed,
-                                sold } = addArtwork;
-                            selectArtwork({
-                                id,
-                                galleryId,
-                                title,
-                                width,
-                                height,
-                                image,
-                                medium,
-                                price,
-                                sold,
-                                framed,
-                            });
-                            const { galleries, artworks } = cache.readQuery({ query: DB_CONTENT });
-                            cache.writeQuery({
-                                query: DB_CONTENT,
-                                data: { galleries, artworks: [...artworks, addArtwork] },
-                            });
-                        }}
-                        refetchQueries={[{
-                            query: GALLERY_ARTWORKS,
-                            variables: {
-                                galleryId: null,
-                            },
-                        }]}
-                    >
-                        {(addArtwork: any, { data, loading, error }: any) => (
-                            <div className="AddArtworks"
-                                onClick={(event) => {
-                                    event.stopPropagation();
-                                    addArtwork();
-                                }}
-                            >
-                                <div className="addArtwork"
-                                >
-                                    <h3> + </h3>
-                                </div>
-                            </div>
-                        )}
-                    </Mutation>
-            )}
-        </AdminContext.Consumer>
-    );
+	return (
+		<AdminContext.Consumer>
+			{({ selectArtwork, selectedGallery }: any) => (
+					<Mutation mutation={ADD_ARTWORK}
+						variables={{
+							galleryId: selectedGallery.id || null,
+						}}
+						update={(cache: any, { data: { addArtwork } }: any) => {
+							// get only needed variables, i.e. no '__typename'
+							const {
+								id,
+								galleryId,
+								title,
+								width,
+								height,
+								image,
+								medium,
+								price,
+								framed,
+								sold } = addArtwork;
+							selectArtwork({
+								framed,
+								galleryId,
+								height,
+								id,
+								image,
+								medium,
+								price,
+								sold,
+								title,
+								width,
+							});
+							const { galleries, artworks } = cache.readQuery({ query: DB_CONTENT });
+							cache.writeQuery({
+								data: { galleries, artworks: [...artworks, addArtwork] },
+								query: DB_CONTENT,
+							});
+						}}
+						refetchQueries={[{
+							query: GALLERY_ARTWORKS,
+							variables: {
+								galleryId: null,
+							},
+						}]}
+					>
+						{(addArtwork: any, { data, loading, error }: any) => (
+							<div className="AddArtworks"
+								onClick={(event) => {
+									event.stopPropagation();
+									addArtwork();
+								}}
+							>
+								<div className="addArtwork"
+								>
+									<h3> + </h3>
+								</div>
+							</div>
+						)}
+					</Mutation>
+			)}
+		</AdminContext.Consumer>
+	);
 };
 
 const ADD_ARTWORK = gql`
-    mutation AddArtwork($galleryId: ID){
-        addArtwork(input: { title: "new artwork", galleryId: $galleryId }) {
-            id
-            galleryId
-            title
-            width
-            height
-            image
-            medium
-            price
-            sold
-            framed
-        }
-    }
+	mutation AddArtwork($galleryId: ID){
+		addArtwork(input: { title: "new artwork", galleryId: $galleryId }) {
+			id
+			galleryId
+			title
+			width
+			height
+			image
+			medium
+			price
+			sold
+			framed
+		}
+	}
 `;
