5ba5c74efec2c7774ecb81fbf8e6ed523da71911
Collin Argo <collinargo@gmail.com>
2019-11-01 21:39:46 -0400

    reset recentlyUpdatedImage after build

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,11 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const Artwork = require("../art-gallery-back/Artwork");
 
+exports.onPostBuild = () => {
+    // reset all the recentlyUpdatedImage things
+    console.log("resetting recentlyUpdatedImage");
+    return async () => await Artwork.upate({ recentlyUpdatedImage: false }, { where: {} });
+}
 // You can delete this file if you're not using it
