8dfb3cd3fe12ebca9378f44ebf46448cefde7c5b
Collin Argo <collinargo@gmail.com>
2019-12-31 16:07:01 -0500

    add gallery selection component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,27 +1,13 @@
 import React from "react";
 
-import LayoutContext from "../contexts/LayoutContext";
 import UpdateFormWrapper from "./gallery/update/UpdateFormWrapper";
 
-import ArtworkChoice from "./gallery/retrieve/ArtworkChoice";
-import GalleryChoice from "./gallery/retrieve/GalleryChoice";
-import GalleryMain from "./gallery/retrieve/GalleryMain";
 import GalleryNavigator from "./gallery/retrieve/GalleryNavigator";
+import GallerySelection from "./gallery/retrieve/GallerySelection";
 
 export default () =>
-	<LayoutContext.Consumer>
-		{({selectedArtwork, selectedGallery}: any) =>
-			<div className="Gallery">
-				<GalleryNavigator></GalleryNavigator>
-				<UpdateFormWrapper></UpdateFormWrapper>
-				<div className="gallerySelection">
-					{!selectedGallery &&
-						<GalleryChoice/>}
-					{!!selectedGallery && !selectedArtwork &&
-						<ArtworkChoice/>}
-					{!!selectedGallery && !!selectedArtwork &&
-						<GalleryMain/>}
-				</div>
-			</div>
-		}
-	</LayoutContext.Consumer>;
+	<div className="Gallery">
+		<GalleryNavigator/>
+		<UpdateFormWrapper/>
+		<GallerySelection/>
+	</div>;
