ff19564eb81a3932bd70a5d24fcf6da198c721a6
Collin Argo <collinargo@gmail.com>
2019-12-31 13:52:24 -0500

    make edit and nav icons clickable

--- src/components/GalleryNavigator.tsx
+++ src/components/GalleryNavigator.tsx
@@ -15,27 +15,31 @@ export default () =>
 						<div className="galleryLocation">
 							<div className="galleryLocationSpecific">
 								{!!selectedGallery &&
-									<FontAwesomeIcon icon={faChevronLeft} size="2x"
+									<FontAwesomeIcon className="galleryNavIcon clickable"
+										icon={faChevronLeft} size="2x"
 										onClick={() => selectGallery()}
 									/>}
 								{!!selectedGallery ?
 									<h3>{selectedGallery.name}</h3> :
 									<h3>collections</h3>}
 								{!!isLoggedIn && !!selectedGallery &&
-									<FontAwesomeIcon icon={faEdit} size="2x"
+									<FontAwesomeIcon className="galleryEditIcon clickable"
+										icon={faEdit} size="2x"
 										onClick={() => editGallery(selectedGallery)}
 									/>}
 							</div>
 							<div className="galleryLocationSpecific">
 								{!!selectedArtwork &&
-									<FontAwesomeIcon icon={faChevronLeft} size="2x"
+									<FontAwesomeIcon className="artworkNavIcon clickable"
+										icon={faChevronLeft} size="2x"
 										onClick={() => selectArtwork()}
 									/>}
 								{!!selectedGallery && (!!selectedArtwork ?
 									<h2>{selectedArtwork.title}</h2> :
 									<h2>artworks</h2>)}
 								{!!isLoggedIn && !!selectedGallery && !!selectedArtwork &&
-									<FontAwesomeIcon icon={faEdit} size="2x"
+									<FontAwesomeIcon className="artworkEditIcon clickable"
+										icon={faEdit} size="2x"
 										onClick={() => editArtwork(selectedArtwork)}
 									/>}
 							</div>
