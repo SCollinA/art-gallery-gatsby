5a71beede5615b377b72f0d58723ecd7766a81c9
Collin Argo <collinargo@gmail.com>
2019-12-31 12:35:44 -0500

    move selected artwork or gallery to gallery component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 
+import LayoutContext from "../contexts/LayoutContext";
+
+import ArtworkChoice from "./ArtworkChoice";
+import GalleryChoice from "./GalleryChoice";
+import GalleryMain from "./GalleryMain";
 import GalleryNavigator from "./GalleryNavigator";
 import UpdateFormWrapper from "./UpdateFormWrapper";
 
 export default () =>
-	<div className="Gallery">
-		<GalleryNavigator></GalleryNavigator>
-		<UpdateFormWrapper></UpdateFormWrapper>
-	</div>;
+	<LayoutContext.Consumer>
+		{({selectedArtwork, selectedGallery}: any) =>
+			<div className="Gallery">
+				<GalleryNavigator></GalleryNavigator>
+				<UpdateFormWrapper></UpdateFormWrapper>
+				<div className="gallerySelection">
+					{!selectedGallery &&
+						<GalleryChoice/>}
+					{!!selectedGallery && !selectedArtwork &&
+						<ArtworkChoice/>}
+					{!!selectedGallery && !!selectedArtwork &&
+						<GalleryMain/>}
+				</div>
+			</div>
+		}
+	</LayoutContext.Consumer>;
