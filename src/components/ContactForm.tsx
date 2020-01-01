22448b43d788bdd473d52d71bf8434e3e8321323
Collin Argo <collinargo@gmail.com>
2020-01-01 14:15:14 -0500

    use useMutation hook in contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { find, get, pipe } from "lodash/fp";
 import React, { useState } from "react";
@@ -11,72 +12,69 @@ import SectionWrapper from "./reusable/SectionWrapper";
 
 export default () => {
 	const [selectedArtwork, setSelectedArtwork] = useState(undefined);
+	const [contactArtist, { loading }] = useMutation(CONTACT_MUTATION);
 	return (
 		<LayoutContext.Consumer>
-			{({ galleries }: any) => (
-				<Mutation mutation={CONTACT_MUTATION}>
-					{(contactArtist: any, { loading }: any) => (
-						<div className="Contact">
-							<Loading loading={loading} fitChild={true}>
-								<SectionWrapper>
-									<form className="contactForm"
-										onSubmit={(event: any) => {
-											event.preventDefault();
-											const artwork = findSelectedArtworkPipe(
-												event.target.artwork.value,
-												get("title"),
-											)(galleries);
-											contactArtist({ variables: {
-												artwork,
-												contactEmail: event.target.email.value,
-												message: event.target.message.value,
-												name: event.target.name.value,
-											}})
-											.then((result: any) => result && window.alert("Thank you for contacting me!"));
-											event.target.reset();
+			{({ galleries }: any) =>
+				<div className="Contact">
+					<Loading loading={loading} fitChild={true}>
+						<SectionWrapper>
+							<form className="contactForm"
+								onSubmit={(event: any) => {
+									event.preventDefault();
+									const artwork = findSelectedArtworkPipe(
+										event.target.artwork.value,
+										get("title"),
+									)(galleries);
+									contactArtist({ variables: {
+										artwork,
+										contactEmail: event.target.email.value,
+										message: event.target.message.value,
+										name: event.target.name.value,
+									}})
+									.then((result: any) => result && window.alert("Thank you for contacting me!"));
+									event.target.reset();
+								}}
+							>
+								<label className="clickable">name
+									<input type="text" name="name" id="name"/>
+								</label>
+								<label className="clickable">e-mail
+									<input type="text" name="email" id="email"/>
+								</label>
+								<label className="contactMessage clickable">message
+									<textarea name="message" id="message"></textarea>
+								</label>
+								<label className="clickable">artwork
+									<select name="artwork" id="artwork"
+										onChange={({target: {value: id}}: any) => {
+											const selectedArtworkOption = findSelectedArtworkPipe(id)(galleries);
+											setSelectedArtwork(selectedArtworkOption);
 										}}
 									>
-										<label className="clickable">name
-											<input type="text" name="name" id="name"/>
-										</label>
-										<label className="clickable">e-mail
-											<input type="text" name="email" id="email"/>
-										</label>
-										<label className="contactMessage clickable">message
-											<textarea name="message" id="message"></textarea>
-										</label>
-										<label className="clickable">artwork
-											<select name="artwork" id="artwork"
-												onChange={({target: {value: id}}: any) => {
-													const selectedArtworkOption = findSelectedArtworkPipe(id)(galleries);
-													setSelectedArtwork(selectedArtworkOption);
-												}}
-											>
-												<option value="-"> - </option>
-												{galleries.map(({ id, name, artworks: artworkOptions }: any) => (
-													<optgroup key={id} label={name}>
-														{artworkOptions.map((artwork: any) => (
-															<option key={artwork.id} value={artwork.id}>
-																{artwork.title}
-															</option>),
-														)}
-													</optgroup>
-												))}
-											</select>
-										</label>
-										<div className="contactArtwork">
-											<ArtworkImage artwork={selectedArtwork}/>
-										</div>
-										<label className="clickable">
-											<input type="submit" value="submit"/>
-										</label>
-									</form>
-								</SectionWrapper>
-							</Loading>
-						</div>
-					)}
-				</Mutation>
-			)}
+										<option value="-"> - </option>
+										{galleries.map(({ id, name, artworks: artworkOptions }: any) => (
+											<optgroup key={id} label={name}>
+												{artworkOptions.map((artwork: any) => (
+													<option key={artwork.id} value={artwork.id}>
+														{artwork.title}
+													</option>),
+												)}
+											</optgroup>
+										))}
+									</select>
+								</label>
+								<div className="contactArtwork">
+									<ArtworkImage artwork={selectedArtwork}/>
+								</div>
+								<label className="clickable">
+									<input type="submit" value="submit"/>
+								</label>
+							</form>
+						</SectionWrapper>
+					</Loading>
+				</div>
+			}
 		</LayoutContext.Consumer>
 	);
 };
